3607d2d3ea4ebaf27a8e1c74843b9da0a6ca9249
fmujkic2 <fmujkic2@etf.unsa.ba>
2020-11-04 15:25:32 +0100

    Kraj zadatka

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -2,22 +2,22 @@ package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
 
-    private double pocetni, krajnji;
+    private double PocetakIntervala, KrajIntervala;
     private boolean poc, kra;
 
 
     Interval(double prvi, double drugi, boolean p, boolean d) {
         if (prvi > drugi) throw new IllegalArgumentException("Pogresan unos");
 
-        this.pocetni = prvi;
-        this.krajnji = drugi;
+        this.PocetakIntervala = prvi;
+        this.KrajIntervala = drugi;
         this.poc = p;
         this.kra = d;
     }
 
     Interval() {
-        this.pocetni = 0;
-        this.krajnji = 0;
+        this.PocetakIntervala = 0;
+        this.KrajIntervala = 0;
         this.poc = false;
         this.kra = false;
     }
@@ -28,23 +28,23 @@ public class Interval {
     }
 
     boolean isIn(double k) {
-        return (k != pocetni || poc) && (!(k < pocetni)) && (k != krajnji || kra) && (!(k > krajnji));
+        return (k != PocetakIntervala || poc) && (!(k < PocetakIntervala)) && (k != KrajIntervala || kra) && (!(k > KrajIntervala));
     }
 
     boolean isNull() {
-        return (this.pocetni == 0 && this.krajnji == 0 && !poc && !kra);
+        return (this.PocetakIntervala == 0 && this.KrajIntervala == 0 && !poc && !kra);
     }
 
     Interval intersect(Interval pom) {
-        if (pom.pocetni == this.pocetni && (!pom.poc || !this.poc)) this.poc = false;
-        else if (pom.pocetni > this.pocetni) {
-            this.pocetni = pom.pocetni;
+        if (pom.PocetakIntervala == this.PocetakIntervala && (!pom.poc || !this.poc)) this.poc = false;
+        else if (pom.PocetakIntervala > this.PocetakIntervala) {
+            this.PocetakIntervala = pom.PocetakIntervala;
             this.poc = pom.poc;
         }
 
-        if (pom.krajnji == this.krajnji && (!pom.kra || !this.kra)) this.kra = false;
-        else if (pom.krajnji < this.krajnji) {
-            this.krajnji = pom.krajnji;
+        if (pom.KrajIntervala == this.KrajIntervala && (!pom.kra || !this.kra)) this.kra = false;
+        else if (pom.KrajIntervala < this.KrajIntervala) {
+            this.KrajIntervala = pom.KrajIntervala;
             this.kra = pom.kra;
         }
 
@@ -58,7 +58,7 @@ public class Interval {
 
     boolean equals(Interval pom) {
 
-        return jednaki(this.pocetni, pom.pocetni) && jednaki(this.krajnji, pom.krajnji) && this.poc == pom.poc && this.kra == pom.kra;
+        return jednaki(this.PocetakIntervala, pom.PocetakIntervala) && jednaki(this.KrajIntervala, pom.KrajIntervala) && this.poc == pom.poc && this.kra == pom.kra;
     }
 
     public String toString() {
@@ -66,10 +66,10 @@ public class Interval {
         if (this.isNull()) return "()";
 
 
-        if (this.poc && this.kra) return "[" + this.pocetni + "," + this.krajnji + "]";
-        else if (!this.poc && this.kra) return "(" + this.pocetni + "," + this.krajnji + "]";
-        else if (this.poc) return "[" + this.pocetni + "," + this.krajnji + ")";
-        else return "(" + this.pocetni + "," + this.krajnji + ")";
+        if (this.poc && this.kra) return "[" + this.PocetakIntervala + "," + this.KrajIntervala + "]";
+        else if (!this.poc && this.kra) return "(" + this.PocetakIntervala + "," + this.KrajIntervala + "]";
+        else if (this.poc) return "[" + this.PocetakIntervala + "," + this.KrajIntervala + ")";
+        else return "(" + this.PocetakIntervala + "," + this.KrajIntervala + ")";
 
     }
 
